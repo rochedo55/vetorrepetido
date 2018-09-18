357a2d9c4480af748b33aec51dcce19323a547e7
rochedo55 <43376647+rochedo55@users.noreply.github.com>
2018-09-18 09:54:36 -0300

    Update q5.c

--- q5.c
+++ q5.c
@@ -6,16 +6,18 @@ int main(void){
 	for(int i=0;i<=n;i++){
 		scanf("%d",&vn[i]);
 	}
-	for(int j=0;j<=vn[n];j++){
-		for(int w=0;w<=vn[n];w++){
-			if(vn[j]==vn[w]){
-				cont++;
+	for(int j=0;j<=n;j++){
+		for(int w=1;w<=n;w++){
+			if(j!=w){
+				if(vn[j]==vn[w]){
+					cont++;
+				}
 			}
 		}
 	}
 	if(cont!=0){
 		printf("sim\n");
 	}else{
-		printf("nao\n");
+		printf("não\n");
 	}
 }
